f7c2b84d228c00656b6516d5fb5954c2fac852cd
CICNETGRP\niran.w <niran_tarlek@hotmail.com>
2022-12-22 15:11:58 +0700

    'update'

--- backend/models/RouteplanSearch.php
+++ backend/models/RouteplanSearch.php
@@ -4,7 +4,7 @@ namespace backend\models;
 
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use backend\models\Routeplan;
+use backend\models\RoutePlan;
 
 /**
  * RouteplanSearch represents the model behind the search form of `backend\models\Routeplan`.
