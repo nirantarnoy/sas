d5bef6254cfc69ed1d53b62af38ef0cfc5d594db
CICNETGRP\niran.w <niran_tarlek@hotmail.com>
2023-06-04 23:54:24 +0700

    'update'

--- backend/views/quotationtitle/_printquotationview.php
+++ backend/views/quotationtitle/_printquotationview.php
@@ -1,54 +1,54 @@
 <?php
 $this->title = "รายละเอียด";
 ?>
-
-<div class="row">
-    <div class="col-lg-12">
-        <h5><?=$model->description?></h5>
+<div id="print-area">
+    <div class="row">
+        <div class="col-lg-12">
+            <h5><?= $model->description ?></h5>
+        </div>
     </div>
-</div>
-<div class="row">
-    <div class="col-lg-12">
-        <table class="table table-bordered table-striped" id="table-list">
-            <thead>
-            <tr>
-                <th>จากคลังสินค้า</th>
-                <th>Route</th>
-                <th>โซนพื้นที่</th>
-                <th>ระยะทาง</th>
-                <th>ปริมาณเฉลี่ยตัน/ปี</th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th style="text-align: right;">ราคาที่เสนอ</th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
+    <div class="row">
+        <div class="col-lg-12">
+            <table class="table table-bordered table-striped" id="table-list">
+                <thead>
+                <tr>
+                    <th>จากคลังสินค้า</th>
+                    <th>Route</th>
+                    <th>โซนพื้นที่</th>
+                    <th>ระยะทาง</th>
+                    <th>ปริมาณเฉลี่ยตัน/ปี</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th style="text-align: right;">ราคาที่เสนอ</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
 
                 <?php if ($model_line != null): ?>
-                    <?php foreach ($model_line as $value):?>
+                    <?php foreach ($model_line as $value): ?>
                         <tr>
                             <td>
 
                             </td>
                             <td>
-                                <?=$value->route_code?>
+                                <?= $value->route_code ?>
                             </td>
                             <td>
 
                             </td>
                             <td>
-                                <?=$value->distance?>
+                                <?= $value->distance ?>
                             </td>
                             <td>
-                               <?=$value->load_qty?>
+                                <?= $value->load_qty ?>
                             </td>
                             <td></td>
                             <td></td>
@@ -56,7 +56,7 @@ $this->title = "รายละเอียด";
                             <td></td>
                             <td></td>
                             <td style="text-align: right;">
-                                <?=$value->price_current_rate?>
+                                <?= $value->price_current_rate ?>
                             </td>
                             <td></td>
                             <td></td>
@@ -64,18 +64,51 @@ $this->title = "รายละเอียด";
                             <td></td>
                             <td></td>
                         </tr>
-                    <?php endforeach;?>
+                    <?php endforeach; ?>
 
-            <?php endif; ?>
+                <?php endif; ?>
 
-            </tbody>
+                </tbody>
 
-        </table>
+            </table>
+        </div>
     </div>
 </div>
 <div class="row">
     <div class="col-lg-12">
-        <div class="btn btn-warning" onclick=""><i class="fa fa-print"></i> พิมพ์</div>
+        <div class="btn btn-warning" onclick="printContent('print-area')"><i class="fa fa-print"></i> พิมพ์</div>
         <div class="btn btn-success" onclick=""><i class="fa fa-file-download"></i> Export</div>
     </div>
 </div>
+<?php
+$js = <<<JS
+$(function(){
+    // var after_save = $(".is-after-save").val();
+    // if(after_save == 1){
+    //     printContent('print-area');
+    //     $("form#form-goto-index").submit();
+    // }
+});
+function printContent(el)
+      {
+         var restorepage = document.body.innerHTML;
+         var printcontent = document.getElementById(el).innerHTML;
+         document.body.innerHTML = printcontent;
+         window.print();
+         document.body.innerHTML = restorepage;
+         
+         // workqueConfirm(e);
+         
+     }
+     
+function confirmwork(){
+   
+    workqueConfirm();
+    // printContent('print-area');
+    // if(confirm("ยืนยันการทำรายการใช่หรือไม่ ?")){
+    //     $("form#form-confirm").submit();
+    // }
+}     
+JS;
+$this->registerJs($js, static::POS_END);
+?>
