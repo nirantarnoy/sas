c725f0fea59f11aa7b42ddb74017592c156a52a8
CICNETGRP\niran.w <niran_tarlek@hotmail.com>
2024-01-29 10:32:20 +0700

    'update'

--- backend/controllers/VendorgroupController.php
+++ backend/controllers/VendorgroupController.php
@@ -25,7 +25,7 @@ class VendorgroupController extends Controller
                 'verbs' => [
                     'class' => VerbFilter::className(),
                     'actions' => [
-                        'delete' => ['POST'],
+                        'delete' => ['POST','GET'],
                     ],
                 ],
             ]
--- backend/controllers/WarehouseController.php
+++ backend/controllers/WarehouseController.php
@@ -25,7 +25,7 @@ class WarehouseController extends Controller
                 'verbs' => [
                     'class' => VerbFilter::className(),
                     'actions' => [
-                        'delete' => ['POST'],
+                        'delete' => ['POST','GET'],
                     ],
                 ],
             ]
