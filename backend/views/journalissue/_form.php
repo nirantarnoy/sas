b6c3687024ad484496fd5b577fb5b55f3d8c2101
Niran Wangyad <niran.w@cicnetgrp.net>
2024-03-03 21:30:06 +0700

    'update'

--- backend/views/journalissue/_form.php
+++ backend/views/journalissue/_form.php
@@ -17,9 +17,9 @@ use yii\widgets\ActiveForm;
                 <?= $form->field($model, 'journal_no')->textInput(['maxlength' => true]) ?>
             </div>
             <div class="col-lg-4">
-                <?php $model->trans_date = $model->isNewRecord ? date('Y-m-d') : date('d-m-Y', strtotime($model->trans_date)) ?>
+                <?php $model->trans_date = $model->isNewRecord ? date('d-m-Y') : date('d-m-Y', strtotime($model->trans_date)) ?>
                 <?= $form->field($model, 'trans_date')->widget(\kartik\date\DatePicker::className(), [
-                    'value' => date('Y-m-d'),
+                    'value' => date('d-m-Y'),
                     'pluginOptions' => [
                         'format' => 'dd-mm-yyyy'
                     ]
