e378410766d0f11740585880598e9fe5e75b5f1b
Niran Wangyad <niran.w@cicnetgrp.net>
2024-03-05 09:16:37 +0700

    'update'

--- backend/views/cashrecord/_form.php
+++ backend/views/cashrecord/_form.php
@@ -146,7 +146,7 @@ $cost_title_data = \common\models\FixcostTitle::find()->where(['type_id' => 1])-
                 <input type="text" class="form-control" readonly
                        value="<?= $model->isNewRecord ? 'Open' : \backend\helpers\CashrecordStatus::getTypeById($model->status) ?>"/>
             </div>
-            <div class="row">
+            <div class="col-lg-3">
                 <?= $form->field($model, 'work_ref_id')->Widget(\kartik\select2\Select2::className(), [
                     'data' => \yii\helpers\ArrayHelper::map(\backend\models\Workorder::find()->where(['status' => 1])->all(), 'id', function ($data) {
                         return $data->workorder_no;
@@ -156,7 +156,7 @@ $cost_title_data = \common\models\FixcostTitle::find()->where(['type_id' => 1])-
                     ]
                 ]) ?>
             </div>
-            <div class="row">
+            <div class="col-lg-3">
                 <?= $form->field($model, 'vat_per')->Widget(\kartik\select2\Select2::className(), [
                     'data' => \yii\helpers\ArrayHelper::map(\backend\helpers\VatperType::asArrayObject(), 'id', 'name'),
                     'options' => [
