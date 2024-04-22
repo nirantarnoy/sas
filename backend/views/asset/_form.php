08ccac39ac7cb53c9cbd69b8af374d1de271681d
Niran Wangyad <niran.w@cicnetgrp.net>
2024-04-22 16:18:09 +0700

    'update'

--- backend/views/asset/_form.php
+++ backend/views/asset/_form.php
@@ -138,32 +138,32 @@ use yii\widgets\ActiveForm;
                 <?php for ($x = 0; $x <= 3; $x++): ?>
                     <?php $loop = -1; ?>
                     <?php foreach ($model_asset_photo as $valuex): ?>
-                        <?php $loop +=1; ?>
+                        <?php $loop += 1; ?>
                         <?php if ($x == 0 && $loop == 0): ?>
                             <div class="col-lg-3"
                                  style="border: 1px dashed grey;border-right: none;height: 250px;text-align: center;">
                                 <img src="<?= \Yii::$app->getUrlManager()->baseUrl . '/uploads/asset_photo/' . $valuex->photo ?>"
-                                     style="width: 130px;margin-top: 5px;" alt="">
+                                     style="max-width: 130px;margin-top: 5px;" alt="">
                             </div>
                             <?php continue 2; ?>
                         <?php elseif ($x == 1 && $loop == 1): ?>
                             <div class="col-lg-3"
                                  style="border: 1px dashed grey;border-right: none;height: 250px;text-align: center;">
                                 <img src="<?= \Yii::$app->getUrlManager()->baseUrl . '/uploads/asset_photo/' . $valuex->photo ?>"
-                                     style="width: 130px;margin-top: 5px;" alt="">
+                                     style="max-width: 130px;margin-top: 5px;" alt="">
                             </div>
                             <?php continue 2; ?>
                         <?php elseif ($x == 2 && $loop == 2): ?>
                             <div class="col-lg-3"
                                  style="border: 1px dashed grey;border-right: none;height: 250px;text-align: center;">
                                 <img src="<?= \Yii::$app->getUrlManager()->baseUrl . '/uploads/asset_photo/' . $valuex->photo ?>"
-                                     style="width: 130px;margin-top: 5px;" alt="">
+                                     style="max-width: 130px;margin-top: 5px;" alt="">
                             </div>
                             <?php continue 2; ?>
                         <?php elseif ($x == 3 && $loop == 3): ?>
                             <div class="col-lg-3" style="border: 1px dashed grey;height: 250px;text-align: center;">
                                 <img src="<?= \Yii::$app->getUrlManager()->baseUrl . '/uploads/asset_photo/' . $valuex->photo ?>"
-                                     style="width: 130px;margin-top: 5px;" alt="">
+                                     style="max-width: 130px;margin-top: 5px;" alt="">
                             </div>
                             <?php continue 2; ?>
                         <?php endif; ?>
