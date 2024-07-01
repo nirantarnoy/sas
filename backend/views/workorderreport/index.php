789c817d6cfd592e88363ca14797299b300204cc
nirantarnoy <niran_tarlek@hotmail.com>
2024-07-01 13:28:57 +0700

    'update'

--- backend/views/workorderreport/index.php
+++ backend/views/workorderreport/index.php
@@ -25,9 +25,9 @@ if($from_date !=null && $to_date != null){
 }else{
     $from_date = date('Y-m-d H:i');
     $to_date = date('Y-m-d H:i');
-    $model_outstanding = \backend\models\Workorder::find()->where(['status' => [1, 3]])->all();
-    $model_recevie_data = \backend\models\Workorder::find()->where(['status' => [3]])->all();
-    $model_closed_data = \backend\models\Workorder::find()->where(['status' => 4])->all();
+    $model_outstanding = \backend\models\Workorder::find()->where(['status' => [1, 3]])->andFilterWhere(['between', 'workorder_date', date('Y-m-d H:i',strtotime($from_date)), date('Y-m-d H:i',strtotime($to_date))])->all();
+    $model_recevie_data = \backend\models\Workorder::find()->where(['status' => [3]])->andFilterWhere(['between', 'workorder_date', date('Y-m-d H:i',strtotime($from_date)), date('Y-m-d H:i',strtotime($to_date))])->all();
+    $model_closed_data = \backend\models\Workorder::find()->where(['status' => 4])->andFilterWhere(['between', 'workorder_date', date('Y-m-d H:i',strtotime($from_date)), date('Y-m-d H:i',strtotime($to_date))])->all();
 }
 
 
